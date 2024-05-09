3f68a543c2052b6979824ecd6d2d59fb6a2420e9
João Vitor Gonçalves Oliveira <joaovitorgoliveira31@gmail.com>
2024-05-09 11:53:30 -0300

    corrige versao ios

--- Sources/Parintins/ColorAsset+swiftUIColor.swift
+++ Sources/Parintins/ColorAsset+swiftUIColor.swift
@@ -2,7 +2,7 @@ import Foundation
 import SwiftUI
 
 public extension ColorAsset {
-    @available(iOS 13.0, macOS 11.0, *)
+    @available(iOS 14.0, macOS 11.0, *)
     var swiftUIColor: SwiftUI.Color {
         get {
             SwiftUI.Color(self.color.cgColor)
