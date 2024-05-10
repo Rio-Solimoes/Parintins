e2d76e03ee51a09cfc4bfbbf185938bf15092a3f
João Vitor Gonçalves Oliveira <joaovitorgoliveira31@gmail.com>
2024-05-10 11:18:09 -0300

    adiciona imagens compartilhadas

---
+++ Sources/Parintins/Shared.swift
@@ -0,0 +1,26 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+// swiftlint:disable superfluous_disable_command file_length implicit_return
+
+// MARK: - Asset Catalogs
+
+// swiftlint:disable identifier_name line_length nesting type_body_length type_name
+public enum Shared {
+  public static let home = ImageAsset(name: "home")
+  public static let work = ImageAsset(name: "work")
+  public static let brothsAndSoups = ImageAsset(name: "brothsAndSoups")
+  public static let desserts = ImageAsset(name: "desserts")
+  public static let drinks = ImageAsset(name: "drinks")
+  public static let entrees = ImageAsset(name: "entrees")
+  public static let fishes = ImageAsset(name: "fishes")
+  public static let garnishes = ImageAsset(name: "garnishes")
+  public static let salads = ImageAsset(name: "salads")
+  public static let seafood = ImageAsset(name: "seafood")
+  public static let emptyDish = ImageAsset(name: "emptyDish")
+  public static let menu = ImageAsset(name: "menu")
+  public static let orders = ImageAsset(name: "orders")
+  public static let search = ImageAsset(name: "search")
+}
+// swiftlint:enable identifier_name line_length nesting type_body_length type_name
+
