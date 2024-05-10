a3a9463557bb18edefa6cc541c4d6cc4e1cc83fa
João Vitor Gonçalves Oliveira <joaovitorgoliveira31@gmail.com>
2024-05-10 08:56:07 -0300

    muda temas para conter assets genéricos

--- Sources/Parintins/Themes.swift
+++ Sources/Parintins/Themes.swift
@@ -12,33 +12,39 @@
   import SwiftUI
 #endif
 
-public protocol ColorTheme {
-    var primary: ColorAsset { get }
-    var secondary: ColorAsset { get }
-}
-
 // swiftlint:disable superfluous_disable_command file_length implicit_return
+
+// MARK: - Theme Protocol
+
+public protocol Theme {
+  var primary: ColorAsset { get }
+  var profileDefault: ImageAsset { get }
+  var secondary: ColorAsset { get }
+}
 
 // MARK: - Asset Catalogs
 
 // swiftlint:disable identifier_name line_length nesting type_body_length type_name
-public enum ColorThemes {
-  public struct Caprichoso: ColorTheme {
+public enum Themes {
+  public struct Caprichoso: Theme {
     public let primary = ColorAsset(name: "Caprichoso/primary")
+    public let profileDefault = ImageAsset(name: "Caprichoso/profileDefault")
     public let secondary = ColorAsset(name: "Caprichoso/secondary")
-      
+
     public init() {}
   }
-  public struct Garantido: ColorTheme {
+  public struct Garantido: Theme {
     public let primary = ColorAsset(name: "Garantido/primary")
+    public let profileDefault = ImageAsset(name: "Garantido/profileDefault")
     public let secondary = ColorAsset(name: "Garantido/secondary")
-      
+
     public init() {}
   }
-  public struct Parintins: ColorTheme {
+  public struct Parintins: Theme {
     public let primary = ColorAsset(name: "Parintins/primary")
+    public let profileDefault = ImageAsset(name: "Parintins/profileDefault")
     public let secondary = ColorAsset(name: "Parintins/secondary")
-      
+
     public init() {}
   }
 }
@@ -106,6 +112,87 @@ public extension SwiftUI.Color {
   init(asset: ColorAsset) {
     let bundle = BundleToken.bundle
     self.init(asset.name, bundle: bundle)
+  }
+}
+#endif
+
+public struct ImageAsset {
+  public fileprivate(set) var name: String
+
+  #if os(macOS)
+  public typealias Image = NSImage
+  #elseif os(iOS) || os(tvOS) || os(watchOS)
+  public typealias Image = UIImage
+  #endif
+
+  @available(iOS 8.0, tvOS 9.0, watchOS 2.0, macOS 10.7, *)
+  public var image: Image {
+    let bundle = BundleToken.bundle
+    #if os(iOS) || os(tvOS)
+    let image = Image(named: name, in: bundle, compatibleWith: nil)
+    #elseif os(macOS)
+    let name = NSImage.Name(self.name)
+    let image = (bundle == .main) ? NSImage(named: name) : bundle.image(forResource: name)
+    #elseif os(watchOS)
+    let image = Image(named: name)
+    #endif
+    guard let result = image else {
+      fatalError("Unable to load image asset named \(name).")
+    }
+    return result
+  }
+
+  #if os(iOS) || os(tvOS)
+  @available(iOS 8.0, tvOS 9.0, *)
+  public func image(compatibleWith traitCollection: UITraitCollection) -> Image {
+    let bundle = BundleToken.bundle
+    guard let result = Image(named: name, in: bundle, compatibleWith: traitCollection) else {
+      fatalError("Unable to load image asset named \(name).")
+    }
+    return result
+  }
+  #endif
+
+  #if canImport(SwiftUI)
+  @available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
+  public var swiftUIImage: SwiftUI.Image {
+    SwiftUI.Image(asset: self)
+  }
+  #endif
+}
+
+public extension ImageAsset.Image {
+  @available(iOS 8.0, tvOS 9.0, watchOS 2.0, *)
+  @available(macOS, deprecated,
+    message: "This initializer is unsafe on macOS, please use the ImageAsset.image property")
+  convenience init?(asset: ImageAsset) {
+    #if os(iOS) || os(tvOS)
+    let bundle = BundleToken.bundle
+    self.init(named: asset.name, in: bundle, compatibleWith: nil)
+    #elseif os(macOS)
+    self.init(named: NSImage.Name(asset.name))
+    #elseif os(watchOS)
+    self.init(named: asset.name)
+    #endif
+  }
+}
+
+#if canImport(SwiftUI)
+@available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
+public extension SwiftUI.Image {
+  init(asset: ImageAsset) {
+    let bundle = BundleToken.bundle
+    self.init(asset.name, bundle: bundle)
+  }
+
+  init(asset: ImageAsset, label: Text) {
+    let bundle = BundleToken.bundle
+    self.init(asset.name, bundle: bundle, label: label)
+  }
+
+  init(decorative asset: ImageAsset) {
+    let bundle = BundleToken.bundle
+    self.init(decorative: asset.name, bundle: bundle)
   }
 }
 #endif
