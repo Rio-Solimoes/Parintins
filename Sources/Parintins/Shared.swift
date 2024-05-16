569611085eb87db4d075fc080f18e211871fd357
João Vitor Gonçalves Oliveira <joaovitorgoliveira31@gmail.com>
2024-05-16 10:31:01 -0300

    separa cores e imagens no Shared

--- Sources/Parintins/Shared.swift
+++ Sources/Parintins/Shared.swift
@@ -7,25 +7,28 @@
 
 // swiftlint:disable identifier_name line_length nesting type_body_length type_name
 public enum Shared {
-  public static let home = ImageAsset(name: "home")
-  public static let work = ImageAsset(name: "work")
-  public static let brothsAndSoups = ImageAsset(name: "brothsAndSoups")
-  public static let desserts = ImageAsset(name: "desserts")
-  public static let drinks = ImageAsset(name: "drinks")
-  public static let entrees = ImageAsset(name: "entrees")
-  public static let fishes = ImageAsset(name: "fishes")
-  public static let garnishes = ImageAsset(name: "garnishes")
-  public static let salads = ImageAsset(name: "salads")
-  public static let seafood = ImageAsset(name: "seafood")
-  public static let emptyDish = ImageAsset(name: "emptyDish")
-  public enum GrayColors {
-    public static let darkGray = ColorAsset(name: "grayColors/darkGray")
-    public static let lightGray = ColorAsset(name: "grayColors/lightGray")
-    public static let mediumGray = ColorAsset(name: "grayColors/mediumGray")
+  public enum Colors {
+    public static let darkGray = ColorAsset(name: "Colors/darkGray")
+    public static let lightGray = ColorAsset(name: "Colors/lightGray")
+    public static let mediumGray = ColorAsset(name: "Colors/mediumGray")
+    public static let highlightGradient = ColorAsset(name: "Colors/highlightGradient")
   }
-  public static let menu = ImageAsset(name: "menu")
-  public static let orders = ImageAsset(name: "orders")
-  public static let search = ImageAsset(name: "search")
+  public enum Images {
+    public static let home = ImageAsset(name: "Images/home")
+    public static let work = ImageAsset(name: "Images/work")
+    public static let brothsAndSoups = ImageAsset(name: "Images/brothsAndSoups")
+    public static let desserts = ImageAsset(name: "Images/desserts")
+    public static let drinks = ImageAsset(name: "Images/drinks")
+    public static let entrees = ImageAsset(name: "Images/entrees")
+    public static let fishes = ImageAsset(name: "Images/fishes")
+    public static let garnishes = ImageAsset(name: "Images/garnishes")
+    public static let salads = ImageAsset(name: "Images/salads")
+    public static let seafood = ImageAsset(name: "Images/seafood")
+    public static let emptyDish = ImageAsset(name: "Images/emptyDish")
+    public static let menu = ImageAsset(name: "Images/menu")
+    public static let orders = ImageAsset(name: "Images/orders")
+    public static let search = ImageAsset(name: "Images/search")
+  }
 }
 // swiftlint:enable identifier_name line_length nesting type_body_length type_name
 
